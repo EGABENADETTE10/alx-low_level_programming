8a77794b4d4dd371109336cefdfca6c1bc94ac6f
EGABENADETTE10 <egabenadette@gmail.com>
2023-06-20 08:12:37 -0700

    main.h

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+int_putchar(Char c);
+void print_alphabet(void);
+void print_alphabet_*10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print _last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print _to_98(int n);
+void print_times_table(int n);
