503bd61b181ba3fd6d1af8d60a7479488d4778a0
EGABENADETTE10 <egabenadette@gmail.com>
2023-06-22 06:36:38 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,11 @@
 #include "main.h"
 /**
 * main - Test function for positive or negative
+* similar to the one worked on C project
 * Return: 0
+* success
+* Betty style used
+* Test file
 */
 int main(void)
 {
